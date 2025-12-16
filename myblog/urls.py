b3878b4a48df4696ae60d17f13f697a7f67e7acd
Ryo Arai <ryo.arai0130@gmail.com>
2025-12-16 12:59:48 +0900

    pushhhhhhh

--- myblog/urls.py
+++ myblog/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
     path("", include("blog.urls")),  # トップページをブログ一覧に
 ]
 
-# 開発環境でのメディアファイル配信
+# 開発環境での静的ファイルとメディアファイル配信
 if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
